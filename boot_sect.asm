5492feb88b3b3c2466d77bab16db31204e0bc04f
cl0wn65536 <hakomoriyuu@gmail.com>
2022-01-10 00:35:54 +0900

    memory and label relations

--- boot_sect.asm
+++ boot_sect.asm
@@ -5,18 +5,26 @@
 
 mov ah, 0x0e
 
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
+mov al, the_secret
 int 0x10
 
+mov al, [the_secret] ; the_secretはセクターの先頭からのインデックスであるのに対し、メモリの読み書きはメモリのスタートからカウントされるので合わない
+; コードの先頭に[org 0x7c00]と入れるとラベル全てにこのオフセットを追加してくれるので正常に動作するようになる。
+int 0x10
+
+mov bx, the_secret
+add bx, 0x7c00 ; コードが読み込まれる位置 + コードの先頭からの文字列までの距離
+mov al, [bx]
+int 0x10
+
+mov al, [0x7c1d] ; アセンブル済のコードから文字列が0x1dにあると判断したので、0x7c00 + 0x1dで0x7c1d
+int 0x10
+
+
 jmp $
+
+the_secret:
+    db "X"
 
 times 510 - ($ - $$) db 0
 
